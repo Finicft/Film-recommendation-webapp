99c910f411793a52e21e0b81b13354a9dac24915
Fangting Chen <fc1@ualberta.ca>
2019-01-13 11:25:27 -0700

    added Movie model and functions to get recom

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.db import connections
 
 
 class Movies(models.Model):
-    imdb_index = models.TextField(blank=True, null=True)
+    imdb_index = models.TextField(blank=True, null=True,primary_key=True)
     imdb_id = models.TextField(blank=True, null=True)
     title = models.TextField(blank=True, null=True)
     year = models.TextField(blank=True, null=True)
@@ -28,3 +29,63 @@ class Movies(models.Model):
     class Meta:
         managed = False
         db_table = 'movies'
+
+    def get_title(self):
+        return self.title
+
+    def get_same_genres(self):
+        movie_genres = [] 
+        same_genre_movies = [] 
+        if self.genre_1 is not "":
+            movie_genres.append(self.genre_1)
+            genre1_movies = Movies.objects.filter(genres__contains = self.genre_1)[:5]
+            same_genre_movies.append(genre1_movies)
+        if self.genre_2 is not "":
+            movie_genres.append(self.genre_2)
+            genre2_movies = Movies.objects.filter(genres__contains = self.genre_2)[:5]
+            same_genre_movies.append(genre2_movies)
+        if self.genre_3 is not "":
+            movie_genres.append(self.genre_3)
+            genre3_movies = Movies.objects.filter(genres__contains = self.genre_3)[:5]
+            same_genre_movies.append(genre3_movies)
+        movies = dict(zip(movie_genres, same_genre_movies))
+        return movies
+    
+    def get_same_actors(self):
+        film_actors = [] 
+        same_actors_films = [] 
+        if self.actor_1 is not "":
+            film_actors.append(self.actor_1)
+            actor_1_movies = Movies.objects.filter(actors__contains = self.actor_1)[:5]
+            same_actors_films.append(actor_1_movies)
+        if self.actor_2 is not "":
+            film_actors.append(self.actor_2)
+            actor_2_movies = Movies.objects.filter(actors__contains = self.actor_2)[:5]
+            same_actors_films.append(actor_2_movies)
+        if self.actor_3 is not "":
+            film_actors.append(self.actor_3)
+            actor_3_movies = Movies.objects.filter(actors__contains = self.actor_3)[:5]
+            same_actors_films.append(actor_3_movies)
+        
+        movies = dict(zip(film_actors,same_actors_films))
+        return movies
+
+    def get_same_directors(self):
+        film_directors = [] 
+        same_director_films = [] 
+        if self.director_1 is not "":
+            film_directors.append(self.director_1)
+            director_1_movies = Movies.objects.filter(directors__contains = self.director_1)[:5]
+            same_director_films.append(director_1_movies)
+        if self.director_2 is not "":
+            film_directors.append(self.director_2)
+            director_2_movies = Movies.objects.filter(directors__contains = self.director_2)[:5]
+            same_director_films.append(director_2_movies)
+        if self.director_3 is not "":
+            film_directors.append(self.director_3)
+            director_2_movies = Movies.objects.filter(directors__contains = self.director_3)[:5]
+            same_director_films.append(director_3_movies)
+        movies = dict(zip(film_directors,same_director_films))
+        return movies
+
+  
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from .models import Movies
+from django.db import connections
 
 def hello(request):
+    movie = Movies.objects.all()
+   
+   
     return HttpResponse("<p>Hello, world</p>")
